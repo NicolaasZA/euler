bdfa0c2e5cb90e2c22b4bb8c592f3988a5815f04
NicolaasZA <lpretorius1995@gmail.com>
2022-03-03 09:39:47 +0200

    problem 45

---
+++ solutions/p45.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import resources.generators as gen
+
+max_n = 5000**2
+
+found = False
+n = 286
+while not found and n < max_n:
+    t = gen.triangle(n)
+
+    if gen.is_hexagonal(t) and gen.is_pentagonal(t):
+        print(t)
+        found = True
+
+    n += 1
